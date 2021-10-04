1fef54d492fa8ab2bad25c09e41124dcd10bd2a0
JongKuk Seo <jongkuk.developer@gmail.com>
2021-10-04 18:30:26 +0900

    first commit

---
+++ practice/Variables_01.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Variables_01 {
+
+	public static void main(String[] args) {
+		//기본 문법을 익히고 기초를 연습하고 응용해보는 공간.
+
+	}
+
+}
---
+++ puzzle/Puzzle001.java
@@ -0,0 +1,11 @@
+package puzzle;
+
+public class Puzzle001 {
+
+	public static void main(String[] args) {
+		// **자료구조/알고리즘** : 응용 연습 / 목표: 논리력, 사고력, 표현력 훈련. 
+		
+
+	}
+
+}
