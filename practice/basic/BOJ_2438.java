88e5929f7b8daf577fa29046e79359e315fdde2b
JongKuk Seo <jongkuk.dev@gmail.com>
2021-11-05 00:01:48 +0900

    BOJ 2438

--- practice/basic/BOJ_2438.java
+++ practice/basic/BOJ_2438.java
@@ -7,6 +7,7 @@ public class BOJ_2438 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		sc.close();
 		for(int i=0;i<N;i++) {
 			
 			for(int j = 0 ;j<=i;j++) {
