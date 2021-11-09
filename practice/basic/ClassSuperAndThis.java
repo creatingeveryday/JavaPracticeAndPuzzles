b16492319a918e8d7f1eb9cf5a4b54a893e7de26
JongKuk Seo <jongkuk.dev@gmail.com>
2021-11-09 23:29:43 +0900

    polymorphism

--- practice/basic/ClassSuperAndThis.java
+++ practice/basic/ClassSuperAndThis.java
@@ -7,12 +7,24 @@ public class ClassSuperAndThis {
 		System.out.println(c.name);
 		System.out.println(c.price);
 		System.out.println(c.weight);
+		System.out.println(c);
 		System.out.println("------------------");
 		Car ca = new Car();
 		System.out.println(ca.name);
 		System.out.println(ca.price);
 		System.out.println(ca.weight);
-
+		System.out.println(ca);
+		
+		Machine m = new Machine();
+		System.out.println(m);
+		
+		Machine ma = new Machine();
+		Machine mau = null;
+		Car car = null;
+		
+//		car = (Car)ma;  // 조상타입객체를 자손타입 참조변수로 접근하면 컴파일 오류 발생. 
+		
+		
 	}
 
 }
@@ -28,6 +40,10 @@ class Machine {
 	public Machine(int weight) {
 		System.out.println("부모 생성자");
 		this.weight = weight;
+	}
+	@Override
+	public String toString() {
+		return "Machine 클래스" +weight;
 	}
 }
 
@@ -48,5 +64,14 @@ class Car extends Machine {
 		this.name = name;
 		this.price = price;
 
+	}
+	@Override
+	public String toString() {
+		
+		return "Car클래스입니다. "+ name +", "+ price +","+weight;
+	}
+	
+	void bbang() {
+		System.out.println("빵빵빵");
 	}
 }
